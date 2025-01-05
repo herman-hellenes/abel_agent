6d50d37772785d14764f7394df2f51143f96e8fd
herman-hellenes <enviroment>
2025-01-05 21:26:56 +0100

    trying render.com

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -1,6 +1,6 @@
-import pysqlite3
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+#import pysqlite3
+#import sys
+#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 from vector_store import FlowerShopVectorStore
